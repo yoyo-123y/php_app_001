21f05a18ee0db396c8525f8ed4304e77deba9ae6
Youssif roushdy <youssifalsayad5656@gmail.com>
2025-09-10 22:03:12 +0300

    finished string category

--- index.php
+++ index.php
@@ -48,7 +48,15 @@
             flex-wrap: wrap;
             width: 50%;
         }
-
+        .textPlace {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 5px;
+            flex-direction: column;
+            flex-wrap: wrap;
+            width: 50%;
+        }
         p {
             color: rgba(229, 255, 0, 1);
             font-size: 25px;
@@ -163,11 +171,56 @@
                     ?>
                 </div>
             </div>
+            <div class="subCont" id="cont7">
+                <h1>Print the username uppercase</h1>
+                <div class="textPlace">
+                    <?php
+                    $userName="hello world";
+                    echo "<p>The original username is $userName</p>";
+                    $u_userName=strtoupper(trim($userName));
+                    echo "<p>The uppercased username is $u_userName</p>";
+                    ?>
+                </div>
+            </div>
+            <div class="subCont" id="cont8">
+                <h1>Print the first 3 digits of a username</h1>
+                <div class="textPlace">
+                    <?php
+                    echo "<p>The original username is $userName</p>";
+                    $s_userName=substr(trim($userName),0,3);
+                    echo "<p>The substred username is $s_userName</p>";
+                    ?>
+                </div>
+            </div>
+            <div class="subCont" id="cont9">
+                <h1>Find the letter a in the <?=$userName;?></h1>
+                <div class="textPlace">
+                    <?php
+                    $l_userName=strtolower(trim($userName));
+                    $r_9=strpos($l_userName,"a");
+                    if($r_9===false) {
+                        echo "<p>The letter is not found</p>";
+                    }else{
+                        echo "<p>The letter is found at index $r_9</p>";
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="subCont" id="cont10">
+                <h1>Replace every space with -</h1>
+                <div class="textPlace">
+                    <?php
+                    $r_userName=str_replace(" ","-",trim($userName));
+                    echo "<p>The original username is $userName</p>";
+                    echo "<p>The replaced username is $r_userName</p>";
+                    ?>
+                </div>
+            </div>
             <div class="button fa fa-caret-right" onclick="button(2)"></div>
         </div>
     </div>
     <script>
-        let numOfSlides=6
+        let numOfSlides=10
         let choosenOne=1
         function button(dir){
             if(dir===1){
